0dd00db0308627d6d53f7d8f690ec1bdd86bc781
showkatbinidris70 <showkatbinidris70@gmail.com>
2024-03-03 21:45:14 +0600

    memo

--- src/components/UseMemo.js
+++ src/components/UseMemo.js
@@ -48,6 +48,10 @@ export default function UseMemo() {
       <p className="py-2 px-3 rounded markdownBgColor">
         <Markdown>{markdown1}</Markdown>
       </p>
+      <p className="fs-5"><span className="text-bold">use memo</span>
+      Unnecessary render যত কমানো যায় তত performace ভালো হবে । Parent Component কে render করলে child component ও render হয় । যা child component, parent component এর ওপর র্নিভরশীল নয় । আর এটাকে বন্ধ করতে হবে useMemo এর মাধ্যমে । তবে এক্কে বারে ছোট কোন component এর ক্ষেত্রে না কারলে ও চলে । বড় application এর ক্ষেত্রে অবশ্যই memo use করতে হবে । memo use করা হয় performance issue solve করার জন্য । যে component render না হলে application এ কোন ধরনের সমস্যা হয় না সে সব ক্ষেত্রে render off করতে হয় memo use করে । আবার যদি কোন component কে render করানোর দরকার হয় তাহলে memo use করার দরকার নাই ।
+যদি কোন state এর value change হয় তাহলে memo use করলে ও state render off করা যায় না । তাহলে বুঝাগেল যে সব component গুলো শুধুমাত্র render করার দরকার নাই সেগুলোতে memo use করা হয় ।
+      </p>
       <h1>Initial Value : {num}</h1>
       <button
         className="btn btn-primary"
